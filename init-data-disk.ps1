9d7b3f626fad76fa9023a4b9c6e379b6b79cb07f
Anders Østhus <anders@proactimasolutions.com>
2025-09-26 18:34:39 +0200

    converted to ms script

--- init-data-disk.ps1
+++ init-data-disk.ps1
@@ -1,21 +1,17 @@
-param(
-  [int]$Lun = 0,                  # Azure LUN of the data disk you attached
-  [char]$DriveLetter = 'F',       # Desired drive letter
-  [string]$Label = 'Data'         # Volume label
-)
+$disks = Get-Disk | Sort-Object Number
+# 83..89 = S..Y
+$letters = 83..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = "datadisk"
 
-$disk = Get-Disk | Where-Object {
-  $_.PartitionStyle -eq 'RAW' -and $_.Location -match "LUN $Lun"
+for($index = 1; $index -lt $disks.Count; $index++) {
+    $driveLetter = $letters[$count].ToString()
+    if ($disks[$index].partitionstyle -eq 'raw') {
+        $disks[$index] | Initialize-Disk -PartitionStyle MBR -PassThru |
+            New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+            Format-Volume -FileSystem NTFS -NewFileSystemLabel "$label.$count" -Confirm:$false -Force
+    } else {
+        $disks[$index] | Get-Partition | Set-Partition -NewDriveLetter $driveLetter
+    }
+    $count++
 }
-
-if (-not $disk) {
-  Write-Host "No RAW disk found for LUN $Lun. Exiting."
-  exit 0
-}
-
-# Initialize, create single partition, assign letter, format NTFS
-$part = Initialize-Disk -Number $disk.Number -PartitionStyle GPT -PassThru |
-        New-Partition -DriveLetter $DriveLetter -UseMaximumSize
-
-Format-Volume -Partition $part -FileSystem NTFS -NewFileSystemLabel $Label -Confirm:$false
-Write-Host "Disk at LUN $Lun initialized as $DriveLetter with label '$Label'."
