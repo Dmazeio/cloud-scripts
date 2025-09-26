991508e9454f25521c3a8380f55ad11f3f8311ca
Anders Østhus <anders@proactimasolutions.com>
2025-09-26 19:13:32 +0200

    ensure folder exists

--- init-data-disk.ps1
+++ init-data-disk.ps1
@@ -4,6 +4,7 @@ if ($disk.partitionstyle -eq 'raw') {
     $disk | Initialize-Disk -PartitionStyle MBR -PassThru |
         New-Partition -UseMaximumSize -DriveLetter $driveLetter |
         Format-Volume -FileSystem NTFS -NewFileSystemLabel "SvcFab" -Confirm:$false -Force
+    New-Item -Path "$($driveLetter):\SvcFab" -ItemType Directory
 } else {
     $disk | Get-Partition | Set-Partition -NewDriveLetter $driveLetter
 }
